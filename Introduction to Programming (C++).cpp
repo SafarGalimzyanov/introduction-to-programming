6334e93f82309245b1132dd8584bc7b7d0c0c286
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2021-12-20 22:00:27 +0300

    A four-digit number is given. Determine whether its decimal notation is symmetric. If the number is symmetric, print 1, otherwise print any other integer. A number can have less than four digits, then consider that its decimal notation is augmented with non-significant zeros on the left.

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
 int main() {
-    int v, t = 0;
-    std::cin >> v >> t;
-    std::cout << (v * t % 109 + 109) % 109;
-    return 0;
+	int n = 0;
+	int A, B, b, a = 0;
+
+	std::cin >> n;
+	A = n / 1000;
+	B = n % 1000 / 100;
+	b = n % 100 / 10;
+	a = n % 10;
+	std::cout << A + B - b - a + A + b - B - a + A - a + B - b + 1;
+
+	return 0;
 }
