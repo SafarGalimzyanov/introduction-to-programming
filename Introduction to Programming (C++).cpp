42221788523409961a9bef2d997b35bc9a315a28
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2021-12-20 22:02:30 +0300

    A snail crawls along a vertical pole of height H meters, ascending A meters in a day and descending B meters in a night. On what day will the snail crawl to the top of the pole?

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -1,15 +1,8 @@
 #include <iostream>
 
 int main() {
-	int n = 0;
-	int A, B, b, a = 0;
-
-	std::cin >> n;
-	A = n / 1000;
-	B = n % 1000 / 100;
-	b = n % 100 / 10;
-	a = n % 10;
-	std::cout << A + B - b - a + A + b - B - a + A - a + B - b + 1;
-
-	return 0;
+    int h, a, b = 0;
+    std::cin >> h >> a >> b;
+    std::cout << (h - b + a - b - 1) / (a - b);
+    return 0;
 }
