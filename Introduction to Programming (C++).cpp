a42db6c1ae876b9308858b98bc88f488585c9add
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2021-12-20 21:51:03 +0300

    The length of the Moscow Ring Road is 109 kilometers. A motorcyclist starts from kilometer zero of the Moscow Ring Road and travels at a speed of V kilometers per hour. At what point will he stop after T hours?

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main() {
-    int h1, m1, s1, h2, m2, s2 = 0;
-    std::cin >> h1 >> m1 >> s1 >> h2 >> m2 >> s2;
-    std::cout << (h2 - h1) * 3600 + (m2 - m1) * 60 + s2 - s1;
+    int v, t = 0;
+    std::cin >> v >> t;
+    std::cout << (v * t % 109 + 109) % 109;
     return 0;
 }
