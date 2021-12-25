b0067251e81b6e0cd500c7c013f698d65ababc73
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2021-12-25 12:13:10 +0300

    Given real numbers a, b, c, d, e, f. Solve the system of linear equations: a*x + b*y = e, c*x + d*y = f. If the system has no solutions, the program should print a single number 0. If the system has infinitely many solutions, each of which has the form y=kx+n, then the program must print the number 1 and then the values of k and n. If the system has only one solution (x0, y0), then the program must output number 2 and then the values of x0 and y0. If the system has infinitely many solutions of the form x=x0, y - any, then the program should output number 3 and then the value of x0. If the system has infinitely many solutions of the form y=y0, x is any, then the program should output number 4 and then the value of y0. If any pair of numbers (x, y) is a solution, the program should output number 5.
    Translated with www.DeepL.com/Translator (free version)

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -1,16 +1,93 @@
 #include <iostream>
-#include <iomanip>
-#include <cmath>
 
 int main() {
-	int sum = 0, num = 0, i = 0;
-	std::cin >> num;
-	while (num != 0)
+
+	double a = 0, b = 0, c = 0, d = 0, e = 0, f = 0;
+	double k = 0, n = 0, x = 0, y = 0, detX = 0, detY = 0, detM;
+	std::cin >> a >> b >> c >> d >> e >> f;
+	detX = (e * d) - (b * f);
+	detY = (a * f) - (c * e);
+	detM = (a * d) - (b * c);
+	if (detM != 0)
 	{
-		sum += num;
-		std::cin >> num;
-		++i;
+		std::cout << "2" << " " << detX / detM << " " << detY / detM;
+		return 0;
 	}
-	std::cout << std::setprecision(11) << std::fixed << double(sum) / i;
+	else
+	{
+		if (detX == 0 && detY == 0)
+		{
+			if (a == 0 && b == 0 && c == 0 && d == 0)
+			{
+				if (e != 0 || f != 0)
+				{
+					std::cout << "0";
+					return 0;
+				}
+				else
+				{
+					std::cout << "5";
+					return 0;
+				}
+			}
+			else
+			{
+				if (a == 0 && c == 0)
+				{
+					if (b != 0)
+					{
+						y = e / b;
+						std::cout << "4" << " " << y;
+						return 0;
+					}
+					else
+					{
+						y = f / d;
+						std::cout << "4" << " " << y;
+						return 0;
+					}
+				}
+				else
+				{
+					if (b == 0 && d == 0)
+					{
+						if (a != 0)
+						{
+							x = e / a;
+							std::cout << "3" << " " << x;
+							return 0;
+						}
+						else
+						{
+							x = f / c;
+							std::cout << "3" << " " << x;
+							return 0;
+						}
+					}
+					if (b != 0)
+					{
+						n = e / b;
+						k = -a / b;
+						std::cout << "1" << " " << k << " " << n;
+						return 0;
+					}
+					else
+					{
+						n = f / d;
+						k = -c / d;
+						std::cout << "1" << " " << k << " " << n;
+						return 0;
+					}
+				}
+			}
+
+		}
+		else
+		{
+			std::cout << "0";
+			return 0;
+		}
+	}
+
 	return 0;
 }
