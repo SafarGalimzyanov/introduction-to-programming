34a3eb17e45e67fa70fb41730316ae5db1d10862
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2021-12-25 12:23:19 +0300

    An array of numbers is given. Count how many pairs of elements in it are equal to each other. Any two elements equal to each other are considered to form one pair to be counted.

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -2,23 +2,42 @@
 #include <vector>
 
 int main() {
-	int n = 0, i = 1;
+	int n = 0, i = 0, temp = 0, j = 0, numOfSameElement = 0, numOfPairs = 0;
 
 	//инициализация вектора
 	std::cin >> n;
 	std::vector <int> a(n);
 
 	//считываем данные в массив:
-	//отсчёт идёт от единицы, чтобы последний элемент с индексом n
-	//занял место n % n = 0
-	for (; i <= n; ++i)
+	for (i = 0; i < n; ++i)
 	{
-		std::cin >> a[i % n];
+		std::cin >> a[i];
 	}
 
-	for (int i : a)
+	//определение числа пар элементов
+	for (i = 0; i < n; ++i)
 	{
-		std::cout << i << " ";
+		//выбран i-ый элемент
+		temp = a[i];
+		for (j = i + 1; j < n; ++j)
+		{
+			//сверяем со всем циклом
+			//кроме i-го и раньше
+			if (temp == a[j])
+			{
+				++numOfSameElement;
+			}
+		}
+		//считаем число пар
+		numOfPairs += numOfSameElement;
+		//обнуляем счётчик одинаковых элементов
+		numOfSameElement = 0;
+
 	}
+
+	//вывод
+	std::cout << numOfPairs;
+
+	return 0;
 	return 0;
 }
