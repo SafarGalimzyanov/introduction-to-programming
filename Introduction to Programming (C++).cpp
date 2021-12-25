739e9a3c44288c86fbd723e86c73daad3a8077eb
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2021-12-25 12:07:49 +0300

    The interest rate on the deposit is P percent per annum, which is added to the amount of the deposit at the end of the year. The deposit is X rubles Y kopecks. Determine the size of the deposit after one year.

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
 int main() {
-    double x = 0;
-    std::cin >> x;
-    std::cout << x - int(x);
-    return 0;
+	int r = 0, k = 0, p = 0;
+	std::cin >> p >> r >> k;
+	k = (r * 100 + k);
+	k = k * (p + 100) / 100;
+	r = k / 100;
+	k = k % 100;
+	std::cout << r << " " << k;
+	return 0;
 }
