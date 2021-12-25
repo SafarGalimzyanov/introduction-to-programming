eba1da9a225c9e21ee7814ddd7e226efd5c08a78
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2021-12-25 12:19:15 +0300

    Output the value of the smallest of all positive elements in the array. It is known that there is at least one positive element in the array.

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -2,34 +2,34 @@
 #include <vector>
 
 int main() {
-	int n = 0, i = 0;
+	int n = 0, i = 0, temp = 0;
+
+	//определение вектора
 	std::cin >> n;
 	std::vector <int> a(n);
-	std::cin >> a[i];
 
-	//проверка на массив размером один
-	if (n == 1)
-	{
-		return 0;
-	}
-	else {}
-	for (i = 1; i < n; ++i)
+	//ищем положительное
+	for (i = 0; i < n; ++i)
 	{
 		std::cin >> a[i];
-		if (a[i] / a[i - 1] > 0)
+		if (a[i] > 0)
 		{
-			if (a[i] > a[i - 1])
-			{
-				std::cout << a[i - 1] << " " << a[i];
-			}
-
-			else
-			{
-				std::cout << a[i] << " " << a[i - 1];
-			}
-			return 0;
+			temp = a[i];
+			//выходим из цикла с найденным нечётным и его индексом
+			break;
 		}
 	}
-
+	//ищем числа меньше найденного в оставшейся последовательности
+	++i; //это чтобы работать со следующим элементом
+	for (; i < n; ++i)
+	{
+		std::cin >> a[i];
+		if (a[i] > 0 && a[i] < temp)
+		{
+			temp = a[i];
+		}
+	}
+	//вывод
+	std::cout << temp;
 	return 0;
 }
