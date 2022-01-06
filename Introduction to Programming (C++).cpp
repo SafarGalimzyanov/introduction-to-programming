dee01f98dcdc2756871c7297cecb5f831a74bc37
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-07 01:43:36 +0300

    Using the given numbers n and m, fill a two-dimensional array of size n×m with numbers from 1 to n×m "in a snake", as shown in the example. Enter two numbers n and m, each number not exceeding 30. Print the resulting array, allotting exactly 4 characters for each element.

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 int main() {
-	int n = 0, m = 0, i = 0, j = 0, k = 0;
+	int n = 0, m = 0, i = 0, j = 0, evenRow = 0;
 
 	//считывание размера вектора
 	std::cin >> n >> m;
@@ -18,28 +18,34 @@ int main() {
 
 	for (; i < n; ++i)
 	{
-		for (j = 0; j < m; ++j)
+		for (j = 0, evenRow = i % 2; j < m; ++j)
 		{
-			std::cin >> array[i][j];
+			//если ряд чётный, 
+			//то остаётся только     этот член, если нечётный, то  этот
+			array[i][j] = i * m + (j + 1) * (1 - evenRow) + (m - j) * (evenRow);
 		}
 	}
-
-	//инициализация транспонированного двумерного вектора
-	std::vector<std::vector<int>> turned(m, std::vector<int>(n));
 
 	for (i = 0; i < n; ++i)
 	{
 		for (j = 0; j < m; ++j)
 		{
-			turned[j][n - i - 1] = array[i][j];
-		}
-	}
+			if (array[i][j] < 10)
+			{
+				//если число однозначное, ставим перед ним два пробела, а после - один
+				std::cout << "  " << array[i][j] << " ";
+			}
+			else if (array[i][j] < 100)
+			{
+				//если число двузначное, ставим перед ним один пробел, а после - один
+				std::cout << " " << array[i][j] << " ";
+			}
+			else
+			{
+				//если число трёхзначное, ставим после него один пробел
+				std::cout << array[i][j] << " ";
+			}
 
-	for (i = 0; i < m; ++i)
-	{
-		for (j = 0; j < n; ++j)
-		{
-			std::cout << turned[i][j] << " ";
 		}
 		std::cout << std::endl;
 	}
