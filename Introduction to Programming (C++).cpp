3cfd36d14690ba64edf1f67c57a27e9d65846e37
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-07 01:49:27 +0300

    Given the numbers n and m, fill a two-dimensional array of size n×m with numbers from 1 to n×m in a spiral that emerges from the top left corner and spirals clockwise, as shown in the example. You enter two numbers n and m that do not exceed 100. Print the resulting array, allotting exactly 4 characters for each element.

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -3,7 +3,7 @@
 
 int main() {
 
-	int n = 0, m = 0, i = 0, j = 0, r = 0, c = 0, value = 1, max = 0;
+	int n = 0, m = 0, i = 0, j = 0, value = 2, max = 0, temp = 0, cnt = 0;
 
 	//считывание размера вектора
 	std::cin >> n >> m;
@@ -15,41 +15,80 @@ int main() {
 	}
 
 	//инициализация двумерного вектора нулями
-	std::vector<std::vector<int>> array(n, std::vector<int>(m));
+	std::vector<std::vector<int>> array(n, std::vector<int>(m, 1));
 
-	for (i = 0; i < n; ++i)
+	//максимальное значение
+	max = n * m;
+	int minI = 0, maxI = n - 1, minJ = 0, maxJ = m - 1;
+
+	for (; true;)
 	{
-		for (j = i % 2; j < m; j += 2, ++value)
+
+		//вправо
+		for (; j < maxJ && value < max + 1; ++value)
 		{
+			++j;
 			array[i][j] = value;
 		}
-	}
+		minI = i + 1;
 
-	//вектор заполнен,
-	//выводим его и выходим из программы
-	{
-		for (i = 0; i < n; ++i)
+		//вниз
+		for (; i < maxI && value < max + 1; ++value)
 		{
-			for (j = 0; j < m; ++j)
-			{
-				if (array[i][j] < 10)
-				{
-					//если число однозначное, ставим перед ним два пробела, а после - один
-					std::cout << "  " << array[i][j] << " ";
-				}
-				else if (array[i][j] < 100)
-				{
-					//если число двузначное, ставим перед ним один пробел, а после - один
-					std::cout << " " << array[i][j] << " ";
-				}
-				else
-				{
-					//если число трёхзначное, ставим после него один пробел
-					std::cout << array[i][j] << " ";
-				}
+			++i;
+			array[i][j] = value;
+		}
+		maxJ = j - 1;
 
+		//влево
+		for (; j > minJ && value < max + 1; ++value)
+		{
+			--j;
+			array[i][j] = value;
+		}
+
+		maxI = i - 1;
+
+
+		//вверх
+		for (; i > minI && value < max + 1; ++value)
+		{
+			--i;
+			array[i][j] = value;
+		}
+
+		minJ = j + 1;
+
+		if (value > max)
+		{
+			//вектор заполнен,
+			//выводим его и выходим из программы
+			{
+				for (i = 0; i < n; ++i)
+				{
+					for (j = 0; j < m; ++j)
+					{
+						if (array[i][j] < 10)
+						{
+							//если число однозначное, ставим перед ним два пробела, а после - один
+							std::cout << "  " << array[i][j] << " ";
+						}
+						else if (array[i][j] < 100)
+						{
+							//если число двузначное, ставим перед ним один пробел, а после - один
+							std::cout << " " << array[i][j] << " ";
+						}
+						else
+						{
+							//если число трёхзначное, ставим после него один пробел
+							std::cout << array[i][j] << " ";
+						}
+
+					}
+					std::cout << std::endl;
+				}
 			}
-			std::cout << std::endl;
+			return 0;
 		}
 	}
 
