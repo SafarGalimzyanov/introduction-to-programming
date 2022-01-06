21c16c5b3206ef79bb2e2ad4b652a0934a7b82ad
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-07 01:38:14 +0300

    Given a number n, not greater than 10, and an array of size n × n. Check whether the array is symmetric with respect to the main diagonal. Print the word "YES" if the array is symmetric, and the word "NO" otherwise.

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -2,47 +2,36 @@
 #include <vector>
 
 int main() {
-	int n = 0, m = 0, i = 0, j = 0, col1 = 0, col2 = 0, temp = 0;
+	int n = 0, i = 0, j = 0;
 
 	//считывание размера вектора
-	std::cin >> n >> m;
+	std::cin >> n;
 
 	//проверка на ввод нулевого вектора
-	if (n < 1 || m < 1)
+	if (n < 1)
 	{
 		return 0;
 	}
 
 	//инициализация двумерного вектора
-	std::vector<std::vector<int>> array(n, std::vector<int>(m));
+	std::vector<std::vector<int>> array(n, std::vector<int>(n));
 
 	for (; i < n; ++i)
 	{
-		for (j = 0; j < m; ++j)
+		for (j = 0; j < n; ++j)
 		{
 			std::cin >> array[i][j];
+			//проверка на неравенство симметричных элементов
+			if (j < i && array[i][j] != array[j][i])
+			{
+				//вывод
+				std::cout << "NO";
+				return 0;
+			}
 		}
-	}
-
-	//считывание номеров столбцов
-	std::cin >> col1 >> col2;
-
-	//смена значений в столбцах
-	for (i = 0; i < n; ++i)
-	{
-		temp = array[i][col1];
-		array[i][col1] = array[i][col2];
-		array[i][col2] = temp;
 	}
 
 	//вывод
-	for (i = 0; i < n; ++i)
-	{
-		for (j = 0; j < m; ++j)
-		{
-			std::cout << array[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
+	std::cout << "YES";
 	return 0;
 }
