97d7231f98afe1b18a7e0a65d5dcb687fe442c1d
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-07 01:37:40 +0300

    Given a two-dimensional array and two numbers: i and j. Swap the columns with numbers i and j in the array. The program receives as input the dimensions of the array n and m, not exceeding 100, then the elements of the array, then the numbers i and j.

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -2,41 +2,43 @@
 #include <vector>
 
 int main() {
-	int n = 0, i = 0, j = 0;
+	int n = 0, m = 0, i = 0, j = 0, col1 = 0, col2 = 0, temp = 0;
 
 	//считывание размера вектора
-	std::cin >> n;
+	std::cin >> n >> m;
 
 	//проверка на ввод нулевого вектора
-	if (n < 1)
+	if (n < 1 || m < 1)
 	{
 		return 0;
 	}
 
 	//инициализация двумерного вектора
-	std::vector<std::vector<int>> array(n, std::vector<int>(n));
+	std::vector<std::vector<int>> array(n, std::vector<int>(m));
 
 	for (; i < n; ++i)
 	{
-		for (j = 0; j < n; ++j)
+		for (j = 0; j < m; ++j)
 		{
-			if (i > j)
-			{
-				array[i][j] = i - j;
-				array[j][i] = i - j;
-			}
-			else
-			{
-				array[i][j] = j - i;
-				array[j][i] = j - i;
-			}
+			std::cin >> array[i][j];
 		}
+	}
+
+	//считывание номеров столбцов
+	std::cin >> col1 >> col2;
+
+	//смена значений в столбцах
+	for (i = 0; i < n; ++i)
+	{
+		temp = array[i][col1];
+		array[i][col1] = array[i][col2];
+		array[i][col2] = temp;
 	}
 
 	//вывод
 	for (i = 0; i < n; ++i)
 	{
-		for (j = 0; j < n; ++j)
+		for (j = 0; j < m; ++j)
 		{
 			std::cout << array[i][j] << " ";
 		}
