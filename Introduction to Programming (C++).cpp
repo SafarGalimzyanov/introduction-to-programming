4e8beabcd05781400fffc7a3eb49516c64c8e167
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-07 19:06:36 +0300

    The teacher gave Petya a homework assignment - in a given text to put the accents in the words, and then asked Vasya to check this homework. Vasya is very unfamiliar with the topic, so he found a dictionary, which indicates how to place the accents in words. Unfortunately, not all the words are in this dictionary. Vasya decided that in words that are not in the dictionary, he would consider that Petya put the accent correctly, if Petya put exactly one accent in this word.
    It turned out that in some words the accent can be placed in more than one way. Vasya decided that in this case, if the way Petya placed the accent corresponds to one of the variants given in the dictionary, he will count it as a correct accentuation, and if it does not correspond, it will be counted as a mistake.
    
    You are given the dictionary Vasya used and the homework Petya turned in. Your task is to find the number of mistakes that Vasya will count in this homework.
    
    First enter the number N - the number of words in the dictionary (0 ≤ N ≤ 20000).
    
    Then comes N lines with words from the dictionary. Each word consists of no more than 30 characters. All words consist of small and capital Latin letters. There is exactly one uppercase letter in each word - the one on which the accent is placed. Words in the dictionary are arranged in alphabetical order. If there are several possibilities for placing the accent in the same word, these variants in the dictionary are in random order.
    
    Next is an exercise done by Petya. The exercise is a line of text, not more than 300000 characters in total. The line consists of words that are separated from each other by exactly one space. The length of each word does not exceed 30 characters. All words consist of small and capital Latin letters (capital letters are the letters above which Petya put the accent). By mistake Petya could have put more than one accent in some word or not put the accent at all.

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -3,56 +3,120 @@
 #include <set>
 #include <string>
 
+//заполнение предложения Пети словами с одной заглавной буквой
+bool fillExc(std::multiset <std::string>& snt, int& err)
+{
+	std::string temp, tempWrd;
+	//считывание строки
+	std::cin >> std::ws; //удаление символа перехода строки
+	std::getline(std::cin, temp);
+	if (temp.empty())
+	{
+		return false;
+	}
+	temp = temp + " "; //чтобы считать последнее слово
+
+	for (int i = temp.find(' '); i != -1;)
+	{
+		tempWrd = temp.substr(0, i);
+
+		//подсчёт заглавных букв в слове
+		int cnt = 0;
+		for (char c : tempWrd)
+		{
+			if (c != char(std::tolower(c)))
+			{
+				++cnt;
+			}
+		}
+
+		//если в слове одна заглавная, то добавляется
+		if (cnt == 1)
+		{
+			snt.insert(tempWrd);
+		}
+		//иначе слово не добавляется
+		else
+		{
+			++err;
+		}
+
+		//удаление добавленного слова
+		temp = temp.substr(i + 1, temp.size());
+
+		//поиск следующего слова
+		i = temp.find(' ');
+	}
+	return true;
+}
+
+//поиск слова  в словаре
+void wordSearch(std::set <std::string>& dic, std::multiset <std::string>& snt, int& err)
+{
+	//цикл по всем словам из предложения Пети
+	for (auto wrdSnt : snt)
+	{
+		//если не найдено точное слово
+		if (dic.find(wrdSnt) == dic.end())
+		{
+			//ищем слова с теми же буквами
+			//превращение всех заглавных букв в строчные
+			std::string temp = "";
+			for (char c : wrdSnt)
+			{
+				temp = temp + char(std::tolower(c));
+			}
+			//если найдено слово со строчными буквами
+			if (dic.find(temp) != dic.end())
+			{
+				++err; //раз оно не точно совпадает, то в нём неверное ударение
+			}
+			//если не найдено слово со строчными буквами, то
+			//этого слова нет в словаре
+		}
+		//если найдено точное слово, то
+		//переход к следующему слову
+	}
+}
+
 int main() {
 
-    //считывание числа строк
-    int n = 0;
-    std::cin >> n;
+	//считывание числа строк
+	int n = 0;
+	std::cin >> n;
 
-    //словарь латино-английский
-    std::map <std::string, std::set <std::string>> dic;
+	//словарь
+	std::set <std::string> dic;
+	//предложение Пети
+	std::multiset <std::string> snt;
 
-    //ввод
-    for (int i = 0; i < n; ++i)
-    {
-        //временные строки для английского и латинского слов
-        std::string eng = "", lat = "";
+	//заполнение словаря
+	for (int i = 0; i < n; ++i)
+	{
+		std::string wrd;
+		std::cin >> wrd;
+		dic.insert(wrd);
+		std::string temp;
+		for (char c : wrd)
+		{
+			temp = temp + char(std::tolower(c));
+		}
+		dic.insert(temp);
+	}
 
-        //любая строка состоит минимум из английского слова, тире и латинского слова
-        std::cin >> eng >> lat >> lat; //считывание тире и его перезаписывание
+	//счётчик ошибок
+	int err = 0;
 
-        //после последнего слова в строке нет запятой, выдаст false
-        while (lat.back() == ',')
-        {
-            //удаляем последний символ (запятую)
-            lat.pop_back();
+	//заполнение предложения Пети
+	if (fillExc(snt, err))
+	{
+		//поиск слов с правильным ударением в словаре
+		wordSearch(dic, snt, err);
+	}
 
-            //добавляем английского слово
-            dic[lat].insert(eng); //слово не добавится, если оно там уже есть
+	//вывод
+	std::cout << err;
 
-            //считывание следующего слова
-            std::cin >> lat;
-        }
 
-        //добавление последнего слова
-        dic[lat].insert(eng);
-    }
-
-    //вывод
-    std::cout << dic.size() << std::endl;
-    for (auto now : dic)
-    {
-        std::cout << now.first << " - ";
-        int count = 1;
-        for (auto meow : now.second) {
-            std::cout << meow;
-            ++count;
-            if (count != now.second.size() + 1) {
-                std::cout << ", ";
-            }
-        }
-        std::cout << std::endl;
-    }
-
-    return 0;
+	return 0;
 }
