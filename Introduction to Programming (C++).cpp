fbbd6536af2bfc3b127e1d3853d05b96335730a1
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-07 18:23:48 +0300

    Given five real numbers: x, y, xc, yc, r. Check if the point (x, y) belongs to a circle with center (xc, yc) and radius r. If the point belongs to the circle, print the word YES, otherwise print the word NO. The solution must contain the function IsPointInCircle(x, y, xc, yc, r), which returns True if the point belongs to the circle and False if it does not. The main program must read the coordinates of the point, call the IsPointInCircle function, and depending on the value returned, display the necessary message. The IsPointInCircle function must not contain an if instruction.

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 
-bool IsPointInSquare(double x, double y)
+bool IsPointInSquare(double x, double y, double xc, double yc, double r)
 {
-	return (x + y <= 1 && -x + y <= 1 && x - y <= 1 && -x - y <= 1);
+	return ((y - yc) * (y - yc) + (x - xc) * (x - xc) <= r * r);
 }
 
 int main() {
 
 	//инициализация переменных
-	double x = 0, y = 0;
+	double x = 0, y = 0, xc = 0, yc = 0, r = 0;
 
 	//считывание значений
-	std::cin >> x >> y;
+	std::cin >> x >> y >> xc >> yc >> r;
 
 	//вывод
-	if (IsPointInSquare(x, y))
+	if (IsPointInSquare(x, y, xc, yc, r))
 	{
 		std::cout << "YES";
 	}
