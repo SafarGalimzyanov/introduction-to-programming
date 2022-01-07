435359f9109605a1d921727980a0d93942a92b94
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-07 19:01:00 +0300

    Given two lists of numbers that can contain up to 100000 numbers each. Count how many numbers are contained in both the first list and the second list at the same time. Enter the number N - the number of elements in the first list, and then N numbers in the first list. Then enter number M - the number of elements of the second list, and then M numbers of the second list.

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -5,19 +5,22 @@ int main() {
 	std::set <int> s;
 	int n;
 	std::cin >> n;
-	for (int i = 0, x = 0; i < n; i++)
+	for (int i = 0, x = 0; i < n; ++i)
+	{
+		std::cin >> x;
+		s.insert(x);
+	}
+	int m, cnt = 0;
+	std::cin >> m;
+	for (int i = 0, x = 0; i < m; ++i)
 	{
 		std::cin >> x;
 		if (s.find(x) != s.end())
 		{
-			std::cout << "YES\n";
+			++cnt;
 		}
-		else
-		{
-			std::cout << "NO\n";
-		}
-		s.insert(x);
 	}
+	std::cout << cnt;
 
 	return 0;
 }
