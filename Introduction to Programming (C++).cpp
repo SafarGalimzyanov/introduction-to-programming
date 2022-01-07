7f904c2cfaec0fe00f29bfc28111ae6298e74a0a
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-07 18:22:44 +0300

    Two real numbers x and y are given. Check if the point with coordinates (x, y) belongs to the shaded square (including its boundary). If the point belongs to the square, print the word YES, otherwise print the word NO. In the picture the grid is laid out in increments of 1. The solution must contain the function IsPointInSquare(x, y), which returns true if the point belongs to the square and false if it doesn't. The main program must read the coordinates of the point, call the IsPointInSquare function, and depending on the value returned, display the necessary message. The IsPointInSquare function must not contain an "if" instruction.

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -1,19 +1,27 @@
 #include <iostream>
-#include <cmath>
 
-double distance(double x1, double y1, double x2, double y2)
+bool IsPointInSquare(double x, double y)
 {
-	return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	return x * x <= 1 && y * y <= 1;
 }
 
 int main() {
 
 	//инициализация переменных
-	double x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+	double x = 0, y = 0;
+
 	//считывание значений
-	std::cin >> x1 >> y1 >> x2 >> y2;
+	std::cin >> x >> y;
+
 	//вывод
-	std::cout << distance(x1, y1, x2, y2);
+	if (IsPointInSquare(x, y))
+	{
+		std::cout << "YES";
+	}
+	else
+	{
+		std::cout << "NO";
+	}
 
 	return 0;
 }
