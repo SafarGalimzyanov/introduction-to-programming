770b989428827171961c7450ee79a534355c5e83
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-07 19:02:04 +0300

    Given two lists of numbers that can contain up to 100000 numbers each.  Output all the numbers that are in both the first and second lists in ascending order. Enter the number N - the number of elements in the first list, and then N numbers in the first list. Then enter number M - the number of elements of the second list, and then M numbers of the second list.

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -2,25 +2,29 @@
 #include <set>
 
 int main() {
-	std::set <int> s;
+	std::set <int> s1, s2;
 	int n;
 	std::cin >> n;
 	for (int i = 0, x = 0; i < n; ++i)
 	{
 		std::cin >> x;
-		s.insert(x);
+		s1.insert(x);
 	}
-	int m, cnt = 0;
+	int m;
 	std::cin >> m;
 	for (int i = 0, x = 0; i < m; ++i)
 	{
 		std::cin >> x;
-		if (s.find(x) != s.end())
+		s2.insert(x);
+	}
+
+	for (auto k : s1)
+	{
+		if (s2.find(k) != s2.end())
 		{
-			++cnt;
+			std::cout << k << " ";
 		}
 	}
-	std::cout << cnt;
 
 	return 0;
 }
