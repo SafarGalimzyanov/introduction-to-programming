c19b84dd4b143be399f5972e327415184533440d
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2022-01-07 18:34:47 +0300

    A skyscraper has n floors. It is known that if you drop a glass ball from the floor number p, and the ball breaks, then if you drop it from the floor number p+1, it will also break. You also know that if you drop the ball from the last floor it always breaks.You want to determine the minimum floor number from which the ball breaks. You have two balls to experiment with. You can smash them all, but as a result you must absolutely determine this number. You need to deduce the smallest number of throws at which you can always solve the problem.

--- Introduction to Programming (C++).cpp
+++ Introduction to Programming (C++).cpp
@@ -1,37 +1,22 @@
 #include <iostream>
-#include <vector>
-
-double fibonacci(int num, std::vector <int>& m)
-{
-	if (num > 2)
-	{
-		if (m[num] > 0)
-		{
-			return m[num];
-		}
-		else
-		{
-			return m[num] = fibonacci(num - 1, m) + fibonacci(num - 2, m);
-		}
-
-	}
-	else
-	{
-		return 1;
-	}
-}
 
 int main() {
 
-	int num = 0;
+	int num = 0, i = 0, k = 0, d = 0;
 
 	//считывание значений
 	std::cin >> num;
 
-	std::vector <int> array(num + 1, 0);
+	for (; k < num - 1; ++k)
+	{
+		for (d = 0; d < i; ++k, ++d)
+		{
+		}
+		++i;
+	}
 
 	//вывод
-	std::cout << fibonacci(num, array);
+	std::cout << i;
 
 	return 0;
 }
